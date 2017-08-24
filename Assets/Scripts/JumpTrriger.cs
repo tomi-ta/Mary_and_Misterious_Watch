2378bb2398c7a200a10421a8ef3503aa46f560ba
tomi-ta <vrl.iwbtg@gmail.com>
2017-08-24 09:04:52 +0900

    tomita
    toriaezu

---
+++ Assets/Scripts/JumpTrriger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpTrriger : PlayerBehaviour {
+
+
+    List<string> jump_trrige_tag;
+    bool jump = false;
+
+    // Use this for initialization
+    void Start () {
+        jump_trrige_tag = Jump_tag;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        foreach (string n in jump_trrige_tag)
+        {
+            if (other.gameObject.CompareTag(n))
+            {
+                jump = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MoveliftBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveliftBehaviour : MonoBehaviour {
+
+    private Vector2 initialPosition;
+    [SerializeField]
+    private float range =3f;
+
+    // Use this for initialization
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        transform.position = new Vector2(initialPosition.x, Mathf.Cos(Time.time * 1.3f) * range + initialPosition.y);
+
+    }
+}
