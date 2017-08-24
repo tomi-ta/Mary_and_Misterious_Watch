83290c5789d1705bdef4d590f0d8d4fd95f4b342
Yuki-12 <yukky.258@gmail.com>
2017-08-24 19:25:25 +0900

    Revert "Revert "Revert "Merge pull request #3 from tomi-ta/hayashi"""
    This reverts commit 198af21e33b74af4d9c25d0cd99bd6722503bd2d.

---
+++ Assets/Scripts/BoxBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxBehaviour : PlayerBehaviour {
+
+    private Rigidbody2D boxrb;
+    [SerializeField]
+    private float bairitu = 1f;
+    private bool boxcollision = false;
+
+    // Use this for initialization
+    void Start () {
+        boxrb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+            boxrb.AddForce(transform.forward * bairitu, ForceMode2D.Impulse);
+            boxcollision = false;
+
+    }
+
+}
