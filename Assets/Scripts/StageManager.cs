6e0ea0761edef96fec6bfca5b143116190d9a427
Yuki-12 <yukky.258@gmail.com>
2017-08-25 16:17:04 +0900

    StageManager

---
+++ Assets/Scripts/StageManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class StageManager: MonoBehaviour
+{
+
+    private int minite;
+    private float second;
+    private int oldSecond;
+    public Text timeLabel;
+
+    // Use this for initialization
+    void Start()
+    {
+        minite = 0;
+        second = 0f;
+        oldSecond = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeScale > 0)
+        {
+            second += Time.deltaTime;
+
+            if (second >= 60.0f)
+            {
+                minite++;
+                second = second - 60;
+            }
+
+            if ((int)second != oldSecond)
+            {
+                timeLabel.text = minite.ToString("00") + ":" + second.ToString("00");
+
+            }
+
+            oldSecond = (int)second;
+        }
+
+    }
+}
