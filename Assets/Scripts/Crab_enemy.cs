c34970ea8fedae3cfec7f57007fb3ec848a5bed0
Yuki-12 <yukky.258@gmail.com>
2017-08-28 20:56:37 +0900

    enemy1

---
+++ Assets/Scripts/Crab_enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crab_enemy : MonoBehaviour {
+
+    Rigidbody2D rb2d;
+    private int moveSpeed = -1;
+
+    // Use this for initialization
+    void Start () { 
+
+        rb2d = GetComponent<Rigidbody2D>();
+       }
+	
+	// Update is called once per frame
+	void Update () {
+
+        rb2d.velocity = new Vector2(transform.localScale.x*moveSpeed, rb2d.velocity.y);
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            Destroy(coll.gameObject);
+        }
+
+        if (coll.gameOject.tag == "move box") 
+        {
+            Vector2 temp = gameObject.transform.localScale;
+            temp.x = 1;
+            gameObject.transform.localScale = temp;
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            Destroy(coll.gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
